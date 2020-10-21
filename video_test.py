483b8bf39506de5d330d195e671cfbe902ae8f1a
CIIRC-Incognite <vavrecka.michal@gmail.com>
2020-10-21 14:57:45 +0200

    vision scripts

---
+++ video_test.py
@@ -0,0 +1,28 @@
+from Tkinter import *
+from PIL import ImageTk, Image
+import cv2
+
+
+root = Tk()
+# Create a frame
+app = Frame(root, bg="white")
+app.grid()
+# Create a label in the frame
+lmain = Label(app)
+lmain.grid()
+
+# Capture from camera
+cap = cv2.VideoCapture(0)
+
+# function for video streaming
+def video_stream():
+    _, frame = cap.read()
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(1, video_stream) 
+
+video_stream()
+root.mainloop()
